0077d9b90e9d2e067a5e962c7f4499d9e80c751d
Daniel <danielATD@gmail.com>
2022-08-18 22:50:59 -0500

    ocean

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'daniel',
-    database : 'OrganizerApp',
-    password: 'sasuke07'
+var db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    database : process.env.DATABASE,
+    password: process.env.PASSWORD
 });
 
 db.connect(function(error){
@@ -15,6 +15,34 @@ db.connect(function(error){
         console.log('Conectado!')
     }
 })
+
+function handleDisconnect() {
+    db = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        database : process.env.DATABASE,
+        password: process.env.PASSWORD
+    }); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    db.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    db.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+  }
+  
+  handleDisconnect();
 
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,40 +1,27 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var expressValidator = require('express-validator');
-var flash = require('express-flash');
-var session = require('express-session');
 var bodyParser = require('body-parser');
 const {validationResult} = require('express-validator/check')
 
 const db = require('./database')
-const port = 2000
+const port = 8080
 const app = express()
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 
-app.use(session({ 
-    secret: '123456cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 }
-}))
  
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/public')
-app.use(flash());
 app.use(expressValidator());
 
    
 
 
-app.listen(port,() =>
+app.listen(process.env.PORT,() =>
     console.info(`Listening on port ${port}`)
 )
 
