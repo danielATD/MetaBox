02cb15dfe6b5105d91edf8a369382fc2e4b91a00
Daniel <danieltejera07@gmail.com>
2022-09-10 20:34:05 -0500

    notenv

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 var db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
+    host: "db-mysql-nyc1-52436-do-user-11776805-0.b.db.ondigitalocean.com",
+    user: "doadmin",
     port: 25060,
-    database : process.env.DATABASE,
-    password: process.env.PASSWORD
+    database : "Users",
+    password: "AVNS_c9FKi5spe2Ys0PLOGLT"
 });
 
 db.getConnection(function(error){
